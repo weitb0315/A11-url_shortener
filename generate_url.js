dda0f9f859032ae0ab0d956cb61631887c2f5b19
weitb0315 <weitb0315@gmail.com>
2023-09-14 21:20:57 +0800

    feat: add generate url function

---
+++ generate_url.js
@@ -0,0 +1,23 @@
+function generateUrl() {
+  // define characters
+  const numbers = '1234567890'
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+
+  // collect all characters
+  let collection = []
+  collection = collection.concat(numbers.split('')).concat(lowerCaseLetters.split('')).concat(upperCaseLetters.split(''))
+
+  // generate 5 characters in random
+  let randomCharacters5 = ''
+  for (let i = 0; i < 5; i++) {
+    randomCharacters5 += collection[Math.floor(Math.random() * collection.length)]
+  }
+
+  // generate url
+  let shorterUrl = 'https://tzbin-url/'
+  shorterUrl += randomCharacters5
+
+  // return the generated url
+  return shorterUrl
+}
