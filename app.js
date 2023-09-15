dc3b8688807c997c772d18bf364eab7cd6e6fc71
weitb0315 <weitb0315@gmail.com>
2023-09-15 14:57:24 +0800

    feat: add index.hbs

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
